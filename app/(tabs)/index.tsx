4b730cd5f52947cfd2c0f30b2c9d15a6166eae31
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-04-06 21:37:17 +0530

    load films from api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,17 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Link } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const {data: movies, loading: moviesLoading, error: moviesError} = useFetch(()=> fetchMovies({query:''}))
 
   return (
     <View className="flex-1 bg-primary">
@@ -22,12 +27,45 @@ export default function Index() {
           className="w-12 h-10 mx-auto mt-20 mb-5"
         />
 
-        <View className="flex-1 mt-5">
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center"/>
+        ): moviesError ? (
+          <Text className="text-white text-center mt-10">{moviesError.message}</Text>
+        ):(
+          <View className="flex-1 mt-5">
           <SearchBar
             onPress={()=>{router.push("/search")}}
-            placeholder="Search movies"
+            placeholder="Search for a movie"
           />
+          <>
+          <Text className="text-white text-2xl font-bold mt-5 mb-3">Lates Movies</Text>
+
+          <FlatList
+            data={movies}
+            renderItem={({item})=>(
+              <MovieCard
+                 {...item}
+              />
+            )}
+            keyExtractor={(item)=> item.id.toString()}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "flex-start",
+              marginBottom: 10,
+              marginRight: 5,
+              gap:20
+            }}
+            className="mt-2 pb-32"
+            scrollEnabled={false}
+          />
+
+
+          </>
+         
         </View>
+        )}
+
+   
 
       </ScrollView>
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}", // Ensure your app directory is included
+    "./app/**/*.{js,jsx,ts,tsx}", './components/**/*.{js,jsx,ts,tsx}' // Ensure your app directory is included
   ],
   theme: {
     extend: {
