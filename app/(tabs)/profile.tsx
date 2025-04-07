2fb613459ab3f63172cc440615f97d9c20192f33
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-04-07 16:14:47 +0530

    movie details

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 function Profile() {
   return (
-    <View className="bg-primary flex-1 px-10">
-      <Text>Profile</Text>
+    <View className="bg-primary flex-1  items-center">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      
     </View>
   );
 }
